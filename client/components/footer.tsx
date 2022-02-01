6d3c6fbe1b06335e6b949f305b85aa200084c4a8
Mark Kraus <vindevccm@gmail.com>
2022-01-31 22:19:42 -0500

    landing page progress

---
+++ client/components/footer.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+export default function Footer(): ReactElement {
+  return <>
+    <footer>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ex iusto assumenda maxime accusamus eum adipisci sint aperiam autem. Reprehenderit natus corrupti ea minus accusamus.
+    </footer>
+  </>;
+}
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
+
+import styles from '../styles/Home.module.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
 export default MyApp;
