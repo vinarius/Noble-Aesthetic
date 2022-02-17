7e418c3aa582257983196fc3029e845426c1792c
Mark Kraus <vindevccm@gmail.com>
2022-02-17 09:12:42 -0500

    updated back to dev after setting master up for coming soon page

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-// import Navbar from '../components/navbar';
-// import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Component {...pageProps} />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
