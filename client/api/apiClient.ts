0cc7110dc32e8f24fd78643c15591ec62be4dde8
Mark Kraus <vindevccm@gmail.com>
2022-03-08 12:32:44 -0500

    updates to clientside api factory

--- client/api/apiClient.ts
+++ client/api/apiClient.ts
@@ -1,7 +1,8 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 export class ApiClient {
   private client: AxiosInstance;
+  private config?: AxiosRequestConfig;
 
   constructor (token?: string) {
     this.client = axios.create({
@@ -11,21 +12,40 @@ export class ApiClient {
         ...token && { Authorization: token } 
       }
     });
+
+    this.config = {
+      headers: {},
+    };
   }
 
-  public get(route: string) {
-    return this.client.get(route);
+  public async get<T>(route: string, options?: AxiosRequestConfig): Promise<T> {
+    return await this.client.get(route, {
+      ...this.config,
+      ...options
+    }).then(res => res.data);
   }
 
-  public post(route: string, data: any) {
-    return this.client.post(route, data);
+  public async post<T>(route: string, body: any, options?: AxiosRequestConfig): Promise<T> {
+    return await this.client.post(route, body, {
+      ...this.config,
+      ...options
+    }).then(res => res.data);
   }
 
-  public put(route: string, data: any) {
-    return this.client.put(route, data);
+  public async put<T>(route: string, body: any, options?: AxiosRequestConfig): Promise<T> {
+    return await this.client.put(route, body, {
+      ...this.config,
+      ...options
+    }).then(res => res.data);
   }
 
-  public delete(route: string, data: any) {
-    return this.client.delete(route, data);
+  public async delete<T>(route: string, body: any, options?: AxiosRequestConfig): Promise<T> {
+    return await this.client.delete(route, {
+      ...this.config,
+      data: {
+        ...body
+      },
+      ...options
+    }).then(res => res.data);
   }
 }
